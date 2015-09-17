6e6b5088744e5efef6c16f1d1fee35d692ab854a
Delisa Mason <iskanamagus@gmail.com>
2015-09-16 17:27:28 -0700

    Add internal representation for breadcrumbs

---
+++ Source/Bugsnag/BugsnagBreadcrumb.h
@@ -0,0 +1,77 @@
+//
+//  BugsnagBreadcrumb.h
+//
+//  Created by Delisa Mason on 9/16/15.
+//
+//  Copyright (c) 2015 Bugsnag, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall remain in place
+// in this source code.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+#import <Foundation/Foundation.h>
+
+#ifndef NS_DESIGNATED_INITIALIZER
+#if __has_attribute(objc_designated_initializer)
+#define NS_DESIGNATED_INITIALIZER __attribute__((objc_designated_initializer))
+#else
+#define NS_DESIGNATED_INITIALIZER
+#endif
+#endif
+
+@interface BugsnagBreadcrumb : NSObject
+
+@property (readonly) NSDate* timestamp;
+@property (readonly,copy) NSString* message;
+
+- (instancetype) initWithMessage:(NSString*)message
+                       timestamp:(NSDate*)date NS_DESIGNATED_INITIALIZER;
+@end
+
+@interface BugsnagBreadcrumbs : NSObject
+
+/** 
+ * The maximum number of breadcrumbs. Resizable. Must be called from the 
+ * main thread.
+ */
+@property (assign,nonatomic,readwrite) NSUInteger capacity;
+
+/** Number of breadcrumbs accumulated */
+@property (assign,readonly) NSUInteger count;
+
+/**
+ * Store a new breadcrumb with a provided message. Must be called from the
+ * main thread.
+ */
+- (void) addBreadcrumb:(NSString*)breadcrumbMessage;
+
+/**
+ * Clear all stored breadcrumbs. Must be called from the main thread.
+ */
+- (void) clearBreadcrumbs;
+
+/** Breadcrumb object for a particular index or nil */
+- (BugsnagBreadcrumb*) objectAtIndexedSubscript:(NSUInteger)index;
+
+/** 
+ * Serializable array representation of breadcrumbs, represented as nested
+ * strings in the format:
+ * [[timestamp,message]...]
+ */
+- (NSArray*) arrayValue;
+
+@end
