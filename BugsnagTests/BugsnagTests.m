0e73f59688b7291cac161e06573bc7dfa84183f1
Simon Maynard <snmaynard@gmail.com>
2015-09-24 11:55:58 -0700

    Comment for test motivation

--- BugsnagTests/BugsnagTests.m
+++ BugsnagTests/BugsnagTests.m
@@ -29,6 +29,8 @@
     XCTAssert(YES, @"Pass");
 }
 
+// Ensure that Bugsnag deals with nil exception names appropriately
+// KSCrash can crash itself if a nil name is passed through
 - (void)testNotifyWithNilName {
     NSString *nilName = nil;
     NSException *exception = [NSException exceptionWithName:nilName reason:nil userInfo:nil];
