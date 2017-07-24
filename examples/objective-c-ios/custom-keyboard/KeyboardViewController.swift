00b076b60d2c9a1095b36b5965e098a97bea3b34
Jamie Lynch <fractalwrench@gmail.com>
2017-07-24 14:29:41 +0100

    update objective-c-ios syntax to swift 3

--- examples/objective-c-ios/custom-keyboard/KeyboardViewController.swift
+++ examples/objective-c-ios/custom-keyboard/KeyboardViewController.swift
@@ -7,44 +7,44 @@ class KeyboardViewController: UIInputViewController {
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        Bugsnag.startBugsnagWithApiKey("YOUR-API-KEY")
+        Bugsnag.start(withApiKey: "YOUR-API-KEY")
     }
 
-    required override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+    required override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        Bugsnag.startBugsnagWithApiKey("YOUR-API-KEY")
+        Bugsnag.start(withApiKey: "YOUR-API-KEY")
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
     
         // Perform custom UI setup here
-        self.nextKeyboardButton = UIButton(type: .System)
+        self.nextKeyboardButton = UIButton(type: .system)
     
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), forState: .Normal)
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: UIControlState())
         self.nextKeyboardButton.sizeToFit()
         self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
     
-        self.nextKeyboardButton.addTarget(self, action: #selector(advanceToNextInputMode), forControlEvents: .TouchUpInside)
+        self.nextKeyboardButton.addTarget(self, action: #selector(advanceToNextInputMode), for: .touchUpInside)
         
         self.view.addSubview(self.nextKeyboardButton)
     
-        self.nextKeyboardButton.leftAnchor.constraintEqualToAnchor(self.view.leftAnchor).active = true
-        self.nextKeyboardButton.bottomAnchor.constraintEqualToAnchor(self.view.bottomAnchor).active = true
+        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
 
         // Perform custom UI setup here
-        self.crashButton = UIButton(type: .System)
+        self.crashButton = UIButton(type: .system)
 
-        self.crashButton.setTitle(NSLocalizedString("Cause crash", comment: "Title for 'Crash' button"), forState: .Normal)
+        self.crashButton.setTitle(NSLocalizedString("Cause crash", comment: "Title for 'Crash' button"), for: UIControlState())
         self.crashButton.sizeToFit()
         self.crashButton.translatesAutoresizingMaskIntoConstraints = false
 
-        self.crashButton.addTarget(self, action: #selector(crashStuff), forControlEvents: .TouchUpInside)
+        self.crashButton.addTarget(self, action: #selector(crashStuff), for: .touchUpInside)
 
         self.view.addSubview(self.crashButton)
 
-        self.crashButton.leftAnchor.constraintEqualToAnchor(self.nextKeyboardButton.rightAnchor).active = true
-        self.crashButton.bottomAnchor.constraintEqualToAnchor(self.view.bottomAnchor).active = true
+        self.crashButton.leftAnchor.constraint(equalTo: self.nextKeyboardButton.rightAnchor).isActive = true
+        self.crashButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -52,21 +52,21 @@ class KeyboardViewController: UIInputViewController {
         // Dispose of any resources that can be recreated
     }
 
-    override func textWillChange(textInput: UITextInput?) {
+    override func textWillChange(_ textInput: UITextInput?) {
         // The app is about to change the document's contents. Perform any preparation here.
     }
 
-    override func textDidChange(textInput: UITextInput?) {
+    override func textDidChange(_ textInput: UITextInput?) {
         // The app has just changed the document's contents, the document context has been updated.
     
         var textColor: UIColor
         let proxy = self.textDocumentProxy
-        if proxy.keyboardAppearance == UIKeyboardAppearance.Dark {
-            textColor = UIColor.whiteColor()
+        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
+            textColor = UIColor.white
         } else {
-            textColor = UIColor.blackColor()
+            textColor = UIColor.black
         }
-        self.nextKeyboardButton.setTitleColor(textColor, forState: .Normal)
+        self.nextKeyboardButton.setTitleColor(textColor, for: UIControlState())
     }
 
     func crashStuff() {
