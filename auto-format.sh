15881c8ea43abc21f0a2cecce719648c64b76a93
fractalwrench <fractalwrench@gmail.com>
2017-10-05 09:32:18 +0100

    format code with clang-format

--- auto-format.sh
+++ auto-format.sh
@@ -1,3 +1,3 @@
-find Source -iname *.h -o -iname *.c -iname .m -iname .mm | xargs clang-format -i;
+find Source -iname *.h -o -iname *.c -iname *.m -iname *.mm | xargs clang-format -i;
 find Tests -iname *.m | xargs clang-format -i;
 
