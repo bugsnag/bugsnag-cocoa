8f6b5ad3f02de8913cec31d8112d33657b05a545
Jamie Lynch <fractalwrench@gmail.com>
2018-01-23 15:28:30 +0000

    Update BugsnagNotifier.m
    remove noisy session tracking log

--- Source/BugsnagNotifier.m
+++ Source/BugsnagNotifier.m
@@ -410,7 +410,6 @@ NSString *const kAppWillTerminate = @"App Will Terminate";
 
 - (void)sessionTick:(id)sender {
     [self.sessionTracker send];
-    NSLog(@"Session Tick!");
 }
 
 - (void)flushPendingReports {
