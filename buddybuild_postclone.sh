912a7766a60b0306cbc2443e421bb0134ad13b34
fractalwrench <fractalwrench@gmail.com>
2018-02-06 14:44:46 +0000

    attempt buddybuild postclone steps for oclint

---
+++ buddybuild_postclone.sh
@@ -0,0 +1,6 @@
+bundle install
+brew cask install oclint
+xcodebuild -project iOS/Bugsnag.xcodeproj -scheme Bugsnag \
+    COMPILER_INDEX_STORE_ENABLE=NO | tee xcodebuild.log | xcpretty -r json-compilation-database -o compile_commands.json
+# Running analysis
+oclint-json-compilation-database
