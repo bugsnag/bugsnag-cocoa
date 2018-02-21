bugsnag/bugsnag-cocoa

commit f62ca6ef6efd3621c428277bad945c435d064e6e

update branch to use unity-specific kscrash enhancements

Source/KSCrash/Source/KSCrash/Recording/BSG_KSCrashReport.c
@@ -2182,7 +2182,10 @@ void bsg_kscrashreport_writeStandardReport(BSG_KSCrash_Context* const crashConte
                                 crashContext->config.crashID,
                                 crashContext->config.processName);
 
-        bsg_kscrw_i_writeBinaryImages(writer, BSG_KSCrashField_BinaryImages);
+        // Don't write the binary images for user reported crashes to improve performance
+        if (crashContext->crash.crashType != BSG_KSCrashTypeUserReported) { // TODO!!!
+            bsg_kscrw_i_writeBinaryImages(writer, BSG_KSCrashField_BinaryImages);
+        }
 
         bsg_kscrw_i_writeProcessState(writer, BSG_KSCrashField_ProcessState);
 
@@ -2205,13 +2208,16 @@ void bsg_kscrashreport_writeStandardReport(BSG_KSCrash_Context* const crashConte
 
         writer->beginObject(writer, BSG_KSCrashField_Crash);
         {
-            bsg_kscrw_i_writeAllThreads(writer,
-                                    BSG_KSCrashField_Threads,
-                                    &crashContext->crash,
-                                    crashContext->config.introspectionRules.enabled,
-                                    crashContext->config.searchThreadNames,
-                                    crashContext->config.searchQueueNames);
-            bsg_kscrw_i_writeError(writer, BSG_KSCrashField_Error, &crashContext->crash);
+            // Don't write the threads for user reported crashes to improve performance
+            if (crashContext->crash.crashType != BSG_KSCrashTypeUserReported) { // TODO!!!
+                bsg_kscrw_i_writeAllThreads(writer,
+                                            BSG_KSCrashField_Threads,
+                                            &crashContext->crash,
+                                            crashContext->config.introspectionRules.enabled,
+                                            crashContext->config.searchThreadNames,
+                                            crashContext->config.searchQueueNames);
+                bsg_kscrw_i_writeError(writer, BSG_KSCrashField_Error, &crashContext->crash);
+            }
         }
         writer->endContainer(writer);
 

Source/Bugsnag.h
@@ -201,4 +201,7 @@ static NSString *_Nonnull const BugsnagSeverityInfo = @"info";
 
 + (NSDateFormatter *_Nonnull)payloadDateFormatter;
 
++ (void)setSuspendThreadsForUserReported:(BOOL)suspendThreadsForUserReported;
++ (void)setReportWhenDebuggerIsAttached:(BOOL)reportWhenDebuggerIsAttached;
+
 @end

Source/Bugsnag.m
@@ -30,6 +30,7 @@
 #import "BugsnagNotifier.h"
 #import "BugsnagSink.h"
 #import "BugsnagLogger.h"
+#import "BSG_KSCrash.h"
 
 static BugsnagNotifier* bsg_g_bugsnag_notifier = NULL;
 
@@ -184,6 +185,14 @@ static BugsnagNotifier* bsg_g_bugsnag_notifier = NULL;
         formatter.dateFormat = @"yyyy'-'MM'-'dd'T'HH':'mm':'ssZZZ";
     });
     return formatter;
+}
+
++ (void)setSuspendThreadsForUserReported:(BOOL)suspendThreadsForUserReported {
+    [[BSG_KSCrash sharedInstance] setSuspendThreadsForUserReported:suspendThreadsForUserReported];
+}
+
++ (void)setReportWhenDebuggerIsAttached:(BOOL)reportWhenDebuggerIsAttached {
+    [[BSG_KSCrash sharedInstance] setReportWhenDebuggerIsAttached:reportWhenDebuggerIsAttached];
 }
 
 @end

Source/KSCrash/Source/KSCrash/Recording/BSG_KSCrash.h
@@ -190,6 +190,21 @@ typedef enum
                   stackTrace:(NSArray*) stackTrace
             terminateProgram:(BOOL) terminateProgram;
 
+/** If YES, user reported exceptions will suspend all threads during report generation.
+  * All threads will be suspended while generating a crash report for a user
+  * reported exception.
+  *
+  * Default: YES
+  */
+@property(nonatomic,readwrite,assign) BOOL suspendThreadsForUserReported;
+
+/** If YES, reports will be sent even if a debugger is attached
+ *
+ * Default: NO
+ */
+@property(nonatomic,readwrite,assign) BOOL reportWhenDebuggerIsAttached;
+
+
 @end
 
 //! Project version number for BSG_KSCrashFramework.

Source/KSCrash/Source/KSCrash/Recording/BSG_KSCrash.m
@@ -107,6 +107,8 @@
 @synthesize catchZombies = _catchZombies;
 @synthesize doNotIntrospectClasses = _doNotIntrospectClasses;
 @synthesize maxStoredReports = _maxStoredReports;
+@synthesize suspendThreadsForUserReported = _suspendThreadsForUserReported;
+@synthesize reportWhenDebuggerIsAttached = _reportWhenDebuggerIsAttached;
 
 
 // ============================================================================
@@ -180,6 +182,8 @@ IMPLEMENT_EXCLUSIVE_SHARED_INSTANCE(BSG_KSCrash)
         self.introspectMemory = YES;
         self.catchZombies = NO;
         self.maxStoredReports = 5;
+        self.suspendThreadsForUserReported = YES;
+        self.reportWhenDebuggerIsAttached = NO;
     }
     return self;
 
@@ -258,6 +262,16 @@ failed:
 {
     _catchZombies = catchZombies;
     bsg_kscrash_setCatchZombies(catchZombies);
+}
+
+- (void)setSuspendThreadsForUserReported:(BOOL) suspendThreadsForUserReported {
+    _suspendThreadsForUserReported = suspendThreadsForUserReported;
+    bsg_kscrash_setSuspendThreadsForUserReported(suspendThreadsForUserReported);
+}
+
+- (void)setReportWhenDebuggerIsAttached:(BOOL)reportWhenDebuggerIsAttached {
+    _reportWhenDebuggerIsAttached = reportWhenDebuggerIsAttached;
+    bsg_kscrash_setReportWhenDebuggerIsAttached(reportWhenDebuggerIsAttached);
 }
 
 - (void) setDoNotIntrospectClasses:(NSArray *)doNotIntrospectClasses

Source/KSCrash/Source/KSCrash/Recording/BSG_KSCrashC.c
@@ -290,3 +290,11 @@ void bsg_kscrash_reportUserException(const char* name,
                                       stackTrace,
                                       terminateProgram);
 }
+
+void bsg_kscrash_setSuspendThreadsForUserReported(bool suspendThreadsForUserReported) {
+    crashContext()->crash.suspendThreadsForUserReported = suspendThreadsForUserReported;
+}
+
+void bsg_kscrash_setReportWhenDebuggerIsAttached(bool reportWhenDebuggerIsAttached) {
+    crashContext()->crash.reportWhenDebuggerIsAttached = reportWhenDebuggerIsAttached;
+}

Source/KSCrash/Source/KSCrash/Recording/BSG_KSCrashC.h
@@ -195,6 +195,22 @@ void bsg_kscrash_reportUserException(const char* name,
                                  const char* lineOfCode,
                                  const char* stackTrace,
                                  bool terminateProgram);
+    
+
+/** If YES, user reported exceptions will suspend all threads during report generation.
+ * All threads will be suspended while generating a crash report for a user
+ * reported exception.
+ *
+ * Default: YES
+ */
+void bsg_kscrash_setSuspendThreadsForUserReported(bool suspendThreadsForUserReported);
+   
+/** If YES, user reported exceptions even if a debugger is attached
+ *
+ * Default: NO
+ */
+void bsg_kscrash_setReportWhenDebuggerIsAttached(bool reportWhenDebuggerIsAttached);
+    
 
 #ifdef __cplusplus
 }

Source/KSCrash/Source/KSCrash/Recording/Sentry/BSG_KSCrashSentry.c
@@ -102,12 +102,15 @@ static bool bsg_g_threads_are_running = true;
 
 BSG_KSCrashType bsg_kscrashsentry_installWithContext(BSG_KSCrash_SentryContext* context,
                                              BSG_KSCrashType crashTypes,
-                                             void (*onCrash)(void))
-{
-    if(bsg_ksmachisBeingTraced())
-    {
-        BSG_KSLOGBASIC_WARN("KSCrash: App is running in a debugger. Only user reported events will be handled.");
-        crashTypes = BSG_KSCrashTypeUserReported;
+                                             void (*onCrash)(void)) {
+    if (bsg_ksmachisBeingTraced()) {
+        if (context->reportWhenDebuggerIsAttached) {
+            BSG_KSLOG_WARN("KSCrash: App is running in a debugger. Crash handling is enabled via configuration.");
+            BSG_KSLOG_INFO("Installing handlers with context %p, crash types 0x%x.", context, crashTypes);
+        } else {
+            BSG_KSLOG_WARN("KSCrash: App is running in a debugger. Only user reported events will be handled.");
+            crashTypes = BSG_KSCrashTypeUserReported;
+        }
     }
     else
     {

commit 4fb5aea1a34421314f366fef1697a7de77516986

fix: Write error info when thread tracing disabled (#245)
Error class and message are not being set for errors when thread tracing
is disabled leading to the error class being set to the default "Exception"
and error message being empty. This change ensures that error class
and message are set regardless of the setting.
 
Fixes regression introduced in f62ca6ef6efd3621c428277bad945c435d064e6e

Source/KSCrash/Source/KSCrash/Recording/BSG_KSCrashReport.c
@@ -2084,9 +2084,9 @@ void bsg_kscrashreport_writeStandardReport(
                     crashContext->config.introspectionRules.enabled,
                     crashContext->config.searchThreadNames,
                     crashContext->config.searchQueueNames);
-                bsg_kscrw_i_writeError(writer, BSG_KSCrashField_Error,
-                                       &crashContext->crash);
             }
+            bsg_kscrw_i_writeError(writer, BSG_KSCrashField_Error,
+                    &crashContext->crash);
         }
         writer->endContainer(writer);
 