f27ff161a95b502f7405098aaeaf0bbeb76c8eac
fractalwrench <fractalwrench@gmail.com>
2018-03-23 14:29:32 +0000

    gcb user-scenarios

---
+++ features/fixtures/ios-swift-cocoapods/iOSTestApp/scenarios/UserDisabledScenario.swift
@@ -0,0 +1,20 @@
+//
+// Created by Jamie Lynch on 23/03/2018.
+// Copyright (c) 2018 Bugsnag. All rights reserved.
+//
+
+import Foundation
+import Bugsnag
+
+/**
+ * Sends a handled error to Bugsnag, which does not include user data.
+ */
+internal class UserDisabledScenario: Scenario {
+
+    override func run() {
+        Bugsnag.configuration()?.setUser(nil, withName: nil, andEmail: nil)
+        let error = NSError(domain: "UserDisabledScenario", code: 100, userInfo: nil)
+        Bugsnag.notifyError(error)
+    }
+
+}
