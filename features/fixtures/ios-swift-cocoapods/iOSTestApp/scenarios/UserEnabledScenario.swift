71cdd76f4f4a1015d543fe05e5da1e5f8e35cfb1
Jamie Lynch <fractalwrench@gmail.com>
2018-03-23 15:00:02 +0000

    Add user scenarios (#253)

---
+++ features/fixtures/ios-swift-cocoapods/iOSTestApp/scenarios/UserEnabledScenario.swift
@@ -0,0 +1,20 @@
+//
+// Created by Jamie Lynch on 23/03/2018.
+// Copyright (c) 2018 Bugsnag. All rights reserved.
+//
+
+import Foundation
+import Bugsnag
+
+/**
+ * Sends a handled error to Bugsnag, which includes user data.
+ */
+internal class UserEnabledScenario: Scenario {
+
+    override func run() {
+        Bugsnag.configuration()?.setUser("123", withName: "Joe Bloggs", andEmail: "user@example.com")
+        let error = NSError(domain: "UserEnabledScenario", code: 100, userInfo: nil)
+        Bugsnag.notifyError(error)
+    }
+
+}
