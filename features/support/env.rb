94a9d882a1593062930f40ad78d9280ed16ee3d9
fractalwrench <fractalwrench@gmail.com>
2018-04-09 11:44:01 +0100

    attempt waiting for 3 mins for simulator

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,7 +12,7 @@ Dir.chdir('features/fixtures/ios-swift-cocoapods') do
     ['bundle', 'exec', 'pod', 'install'],
     ['../../scripts/build_ios_app.sh'],
     ['../../scripts/launch_ios_simulators.sh'],
-    ['sleep', '60']
+    ['sleep', '180']
   ])
 end
 
