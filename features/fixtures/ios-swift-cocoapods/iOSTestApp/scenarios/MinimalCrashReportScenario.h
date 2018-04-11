f649c344e885080a35769e76e406843999dad3e5
Jamie Lynch <fractalwrench@gmail.com>
2018-04-11 23:41:40 +0100

    Add scenario which crashes within the crash handler (#268)

---
+++ features/fixtures/ios-swift-cocoapods/iOSTestApp/scenarios/MinimalCrashReportScenario.h
@@ -0,0 +1,11 @@
+//
+// Created by Jamie Lynch on 28/03/2018.
+// Copyright (c) 2018 Bugsnag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Scenario.h"
+
+
+@interface MinimalCrashReportScenario : Scenario
+@end
