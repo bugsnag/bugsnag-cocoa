370cd33ba827f744344855a5bab2db681a799433
Jamie Lynch <fractalwrench@gmail.com>
2018-04-18 22:47:58 +0100

    tests: Add __builtin_trap scenario (#272)

---
+++ features/fixtures/ios-swift-cocoapods/iOSTestApp/scenarios/BuiltinTrapScenario.h
@@ -0,0 +1,11 @@
+//
+// Created by Jamie Lynch on 12/04/2018.
+// Copyright (c) 2018 Bugsnag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Scenario.h"
+
+
+@interface BuiltinTrapScenario : Scenario
+@end
