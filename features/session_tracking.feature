a9e30b98009443c0a08d2c0e9030f60f789a6895
Delisa Mason <iskanamagus@gmail.com>
2018-07-13 19:46:46 +0100

    tests: Add checks for handled counts in payloads

--- features/session_tracking.feature
+++ features/session_tracking.feature
@@ -58,3 +58,19 @@ Scenario: Disabling auto-capture sends no sessions
     When I run "DisabledSessionTrackingScenario" with the defaults on "iPhone8-11.2"
     And I wait for 10 seconds
     Then I should receive 0 requests
+
+Scenario: Encountering a handled event during a session
+    When I run "AutoSessionHandledEventsScenario" with the defaults on "iPhone8-11.2"
+    And I wait for 10 seconds
+    Then I should receive 3 requests
+    And request 0 is valid for the session tracking API
+    And request 1 is valid for the error reporting API
+    And request 2 is valid for the error reporting API
+
+    And the payload field "events" is an array with 1 element for request 1
+    And the payload field "events.0.session.events.handled" equals 1 for request 1
+    And the payload field "events.0.session.id" of request 1 equals the payload field "sessions.0.id" of request 0
+
+    And the payload field "events" is an array with 1 element for request 2
+    And the payload field "events.0.session.events.handled" equals 2 for request 2
+    And the payload field "events.0.session.id" of request 2 equals the payload field "sessions.0.id" of request 0
--- features/steps/ios_steps.rb
+++ features/steps/ios_steps.rb
@@ -35,3 +35,44 @@ When("I crash the app using {string}") do |event|
     And I set environment variable "EVENT_MODE" to "noevent"
   }
 end
+
+# TODO: move upstream into maze-runner
+Then("request {int} is valid for the session tracking API") do |index|
+  steps %Q{
+    Then the "Bugsnag-API-Key" header is not null for request #{index}
+    And the "Content-Type" header equals "application/json" for request #{index}
+    And the "Bugsnag-Payload-Version" header equals "1.0" for request #{index}
+    And the "Bugsnag-Sent-At" header is a timestamp for request #{index}
+
+    And the payload field "app" is not null for request #{index}
+    And the payload field "device" is not null for request #{index}
+    And the payload field "notifier.name" is not null for request #{index}
+    And the payload field "notifier.url" is not null for request #{index}
+    And the payload field "notifier.version" is not null for request #{index}
+    And the payload has a valid sessions array for request #{index}
+  }
+end
+
+Then("request {int} is valid for the error reporting API") do |index|
+  steps %Q{
+    Then the "Bugsnag-API-Key" header is not null for request #{index}
+    And the "Content-Type" header equals "application/json" for request #{index}
+    And the "Bugsnag-Sent-At" header is a timestamp for request #{index}
+
+    And the payload field "notifier.name" is not null for request #{index}
+    And the payload field "notifier.url" is not null for request #{index}
+    And the payload field "notifier.version" is not null for request #{index}
+    And the payload field "events" is a non-empty array for request #{index}
+
+    And each element in payload field "events" has "severity" for request #{index}
+    And each element in payload field "events" has "severityReason.type" for request #{index}
+    And each element in payload field "events" has "unhandled" for request #{index}
+    And each element in payload field "events" has "exceptions" for request #{index}
+  }
+end
+
+Then("the payload field {string} of request {int} equals the payload field {string} of request {int}") do |field1, request_index1, field2, request_index2|
+  value1 = read_key_path(find_request(request_index1)[:body], field1)
+  value2 = read_key_path(find_request(request_index2)[:body], field2)
+  assert_equal(value1, value2)
+end
