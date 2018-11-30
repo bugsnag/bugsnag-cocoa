8aac604ee60036d8e86c8a3df9642df752e66e31
Delisa Mason <iskanamagus@gmail.com>
2018-11-30 12:02:58 -0800

    build: Remove autoformatter script
    Formatting should be changed as a part of a changeset rather than
    globally

--- auto-format.sh
+++
@@ -1,6 +0,0 @@
-find Source -iname *.h | xargs clang-format -i;
-find Source -iname *.m | xargs clang-format -i;
-find Source -iname *.c | xargs clang-format -i;
-find Source -iname *.mm | xargs clang-format -i;
-find Tests -iname *.m | xargs clang-format -i;
-
