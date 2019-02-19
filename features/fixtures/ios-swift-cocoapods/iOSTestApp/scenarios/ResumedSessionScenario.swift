d9c76977cd3e2306c659aa64859f8de550d50429
fractalwrench <fractalwrench@gmail.com>
2019-02-19 14:09:55 +0000

    test: add mazerunner scenarios for verifying stopSession and resumeSession behaviour

---
+++ features/fixtures/ios-swift-cocoapods/iOSTestApp/scenarios/ResumedSessionScenario.swift
@@ -0,0 +1,28 @@
+//
+//  ResumedSessionScenario.swift
+//  iOSTestApp
+//
+//  Created by Jamie Lynch on 19/02/2019.
+//  Copyright © 2019 Bugsnag. All rights reserved.
+//
+
+import Foundation
+import Bugsnag
+
+internal class ResumedSessionScenario: Scenario {
+    override func startBugsnag() {
+        self.config.shouldAutoCaptureSessions = false;
+        super.startBugsnag()
+    }
+
+    override func run() {
+        // send 1st exception
+        Bugsnag.startSession()
+        Bugsnag.notifyError(NSError(domain: "First error", code: 101, userInfo: nil))
+
+        // send 2nd exception after resuming a session
+        Bugsnag.stopSession()
+        Bugsnag.resumeSession()
+        Bugsnag.notifyError(NSError(domain: "Second error", code: 101, userInfo: nil))
+    }
+}
