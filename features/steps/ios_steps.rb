3cb0c7dda34e6a377e28d5ef0bd9c88f144a2bb6
Delisa Mason <iskanamagus@gmail.com>
2019-02-20 14:19:19 +0000

    tests: Rework minimal report example to expect two reports
    The change to filenames means that both crashes can now be reported
    since they will use different paths and avoid overwriting the other.

--- features/steps/ios_steps.rb
+++ features/steps/ios_steps.rb
@@ -52,7 +52,17 @@ When("I corrupt all reports on disk") do
   end
 end
 
-Then("each event in the payload for request {int} matches one of:") do |request_index, table|
+Then("each event in the payload matches one of:") do |table|
+  # Checks string equality of event fields against values
+  events = read_key_path(find_request(0)[:body], "events")
+  table.hashes.each do |values|
+    assert_not_nil(events.detect do |event|
+      values.all? {|k,v| v == read_key_path(event, k) }
+    end, "No event matches the following values: #{values}")
+  end
+end
+
+Then("each event with a session in the payload for request {int} matches one of:") do |request_index, table|
   events = read_key_path(find_request(request_index)[:body], "events")
   table.hashes.each do |values|
     assert_not_nil(events.detect do |event|
