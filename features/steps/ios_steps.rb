748256820b9a573fbab0e68dbbf3c0ce51ca4f19
Delisa Mason <iskanamagus@gmail.com>
2019-06-07 17:18:56 +0100

    ci: Wait for slow tests

--- features/steps/ios_steps.rb
+++ features/steps/ios_steps.rb
@@ -1,4 +1,7 @@
+SLOW_CI_TESTS = ['PrivilegedInstructionScenario', 'BuiltinTrapScenario']
+
 When("I run {string}") do |event_type|
+  @scenario_class = event_type
   wait_time = '4'
   steps %Q{
     When I set environment variable "BUGSNAG_API_KEY" to "a35a2a72bd230ac0aa0f52715bbdc6aa"
@@ -9,7 +12,8 @@ When("I run {string}") do |event_type|
 end
 
 When("I launch the app") do
-  wait_time = '4'
+  wait_time = 4
+  wait_time += 10 if RUNNING_CI && SLOW_CI_TESTS.include?(@scenario_class)
   steps %Q{
     When I run the script "features/scripts/launch_ios_app.sh"
     And I wait for #{wait_time} seconds
@@ -17,12 +21,14 @@ When("I launch the app") do
 end
 When("I relaunch the app") do
   wait_time = RUNNING_CI ? 20 : 5
+  wait_time += 40 if RUNNING_CI && SLOW_CI_TESTS.include?(@scenario_class)
   steps %Q{
     When I run the script "features/scripts/launch_ios_app.sh"
     And I wait for #{wait_time} seconds
   }
 end
 When("I crash the app using {string}") do |event|
+  @scenario_class = event
   steps %Q{
     When I set environment variable "EVENT_TYPE" to "#{event}"
     And I set environment variable "EVENT_MODE" to "normal"
