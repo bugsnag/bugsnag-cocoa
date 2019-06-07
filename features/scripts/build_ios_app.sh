8005e9122860618db26b7f89bcd6dccecc3232a1
Delisa Mason <iskanamagus@gmail.com>
2019-06-07 15:47:30 +0100

    tests(maze): Bump end-to-end test sdk version
    Updates assertion for malloc corruption

--- features/scripts/build_ios_app.sh
+++ features/scripts/build_ios_app.sh
@@ -9,7 +9,7 @@ xcrun xcodebuild \
   -scheme iOSTestApp \
   -workspace iOSTestApp.xcworkspace \
   -configuration Debug \
-  -destination 'platform=iOS Simulator,name=iPhone 8,OS=11.2' \
+  -destination 'platform=iOS Simulator,name=iPhone 8,OS=12.1' \
   -derivedDataPath build \
   -quiet \
   clean build
