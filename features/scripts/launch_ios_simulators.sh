47b935037549c589611fb589f1e48d096ea16171
Paul Zabelin <paulz@users.noreply.github.com>
2019-06-10 14:41:10 -0700

    support selecting simulators independent of xcode version

--- features/scripts/launch_ios_simulators.sh
+++ features/scripts/launch_ios_simulators.sh
@@ -3,13 +3,8 @@
 INSTALL_PATH=build/Build/Products/Debug-iphonesimulator/iOSTestApp.app
 HOST_OS_VERSION=$(sw_vers | grep ProductVersion)
 OS_VERSION=${MAZE_SDK:="12.1"}
-
-if [[ $HOST_OS_VERSION == "ProductVersion: 10.13"* ]]; then
-SIM_DEVICE="iPhone 8"
-else
 OS_VERSION=com.apple.CoreSimulator.SimRuntime.iOS-"${OS_VERSION//\./$'-'}"
-SIM_DEVICE=com.apple.CoreSimulator.SimDeviceType.iPhone-8
-fi
+SIM_DEVICE="iPhone 8"
 
 # Create required simulators
 xcrun simctl create "maze-sim" "$SIM_DEVICE" "$OS_VERSION"
