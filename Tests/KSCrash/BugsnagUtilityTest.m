3611c6b6e97e6c0132bc59fa26b1ba0808ed60df
Paul Zabelin <paulz@users.noreply.github.com>
2019-07-01 10:38:13 -0700

    add unit test for BSGDictSafeSet

---
+++ Tests/KSCrash/BugsnagUtilityTest.m
@@ -0,0 +1,36 @@
+//
+//  BugsnagUtilityTest.m
+//  Tests
+//
+//  Created by Paul Zabelin on 7/1/19.
+//  Copyright © 2019 Bugsnag. All rights reserved.
+//
+
+@import XCTest;
+#import "BugsnagUtility.h"
+
+@interface BugsnagUtilityTest : XCTestCase
+@property (nonatomic, strong) NSMutableDictionary *dict;
+@end
+
+@implementation BugsnagUtilityTest
+
+- (void)setUp {
+    self.dict = [@{} mutableCopy];
+}
+
+- (void)tearDown {
+    self.dict = nil;
+}
+
+- (void)testBSGDictSafeSetNil {
+    BSGDictSafeSet(self.dict, @"test", nil);
+    XCTAssertEqual(self.dict[@"test"], [NSNull null], @"should store NSNull for nil");
+}
+
+- (void)testBSGDictSafeSetNotNil {
+    BSGDictSafeSet(self.dict, @"test", @"example");
+    XCTAssertEqual(self.dict[@"test"], @"example", @"should store example string");
+}
+
+@end
