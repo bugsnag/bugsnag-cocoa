71df547efc645e915f056eb9714c15249ea3be04
Alex Moinet <alex@bugsnag.com>
2019-09-18 16:08:36 +0100

    Buildkite-move: Add exit code checks for each build step

--- end-to-end-tests/scripts/export_ios_app.sh
+++ end-to-end-tests/scripts/export_ios_app.sh
@@ -9,6 +9,11 @@ rm -rf output
 
 pod install
 
+if [ $? -eq 0 ]; then
+  echo "Pods could not be installed"
+  exit 1
+fi
+
 xcrun xcodebuild \
   -scheme iOSTestApp \
   -workspace iOSTestApp.xcworkspace \
@@ -18,11 +23,21 @@ xcrun xcodebuild \
   -quiet \
   archive
 
+if [ $? -eq 0 ]; then
+  echo "App could not be exported"
+  exit 1
+fi
+
 xcrun xcodebuild -exportArchive \
   -archivePath archive/iosTestApp.xcarchive \
   -exportPath output/ \
   -quiet \
   -exportOptionsPlist exportOptions.plist
+
+if [ $? -eq 0 ]; then
+  echo "Archive could not be created"
+  exit 1
+fi
 
 cd ../../
 
