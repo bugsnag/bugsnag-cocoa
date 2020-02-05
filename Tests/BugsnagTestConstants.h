0400c6e76416a47a3e378c6fde9f67a7fef0e6e7
Robin Macharg <robin.macharg+github@gmail.com>
2020-02-05 17:10:51 +0000

    feat: add per-BugsnagEvent mutable apiKey

--- Tests/BugsnagTestConstants.h
+++ Tests/BugsnagTestConstants.h
@@ -17,6 +17,8 @@
 // One string to rule(r) them all:                         12345678901234567890123456789012345678901234567890
 static NSString * _Nonnull const DUMMY_APIKEY_32CHAR_1 = @"0192837465afbecd0192837465afbecd"; // the correct length
 static NSString * _Nonnull const DUMMY_APIKEY_32CHAR_2 = @"aabbccddeeff00112233445566778899";
+static NSString * _Nonnull const DUMMY_APIKEY_32CHAR_3 = @"99887766554433221100ffeeddccbbaa";
+static NSString * _Nonnull const DUMMY_APIKEY_32CHAR_4 = @"98765432109876543210abcdefabcdef";
 static NSString * _Nonnull const DUMMY_APIKEY_16CHAR   = @"0192837465afbecd"; // too short
 static NSString * _Nonnull const DUMMY_APIKEY_48CHAR   = @"0192837465afbecd0192837465afbecd0192837465afbecd"; // too long
 
