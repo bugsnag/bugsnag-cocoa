1a25cd913c849a42eb52ccdf56864cba998261f8
Robin Macharg <robin.macharg+github@gmail.com>
2020-02-25 20:10:31 +0000

    Added Vendored SSKeychain to MacOS and tvOS projects, fixed test user persistence setup handling

--- Tests/BugsnagBaseUnitTest.m
+++ Tests/BugsnagBaseUnitTest.m
@@ -25,10 +25,25 @@
  *   - send to an arbitrary non-functional endpoint.
  *
  * We take the former approach.
+ *
+ * @param willNotify Whether the notifier should actually send the event to the server
+ * @param willPersistUser Whether any user information should be persisted
  */
--(void)setUpBugsnagWillCallNotify:(bool)willNotify {
+
+-(void)setUpBugsnagWillCallNotify:(bool)willNotify
+{
+    // Default to not persisting user info
+    [self setUpBugsnagWillCallNotify:willNotify
+                      andPersistUser:false];
+}
+
+-(void)setUpBugsnagWillCallNotify:(bool)willNotify
+                   andPersistUser:(bool)willPersistUser
+{
     NSError *error;
     BugsnagConfiguration *configuration = [[BugsnagConfiguration alloc] initWithApiKey:DUMMY_APIKEY_32CHAR_1 error:&error];
+    [configuration setPersistUser:willPersistUser];
+    
     if (willNotify) {
         [configuration addOnSendBlock:^bool(NSDictionary * _Nonnull rawEventData,
                                                 BugsnagEvent * _Nonnull reports)
