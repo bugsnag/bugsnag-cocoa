3c782261fa3e5276dc001163d483136989f10621
fractalwrench <fractalwrench@gmail.com>
2020-03-30 14:41:31 +0100

    feat: make BugsnagClient public interface

---
+++ Tests/BugsnagClientMirrorTest.m
@@ -0,0 +1,26 @@
+//
+//  BugsnagClientMirrorTest.m
+//  Tests
+//
+//  Created by Jamie Lynch on 30/03/2020.
+//  Copyright © 2020 Bugsnag. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BugsnagClientMirrorTest : XCTestCase
+
+@end
+
+@implementation BugsnagClientMirrorTest
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
