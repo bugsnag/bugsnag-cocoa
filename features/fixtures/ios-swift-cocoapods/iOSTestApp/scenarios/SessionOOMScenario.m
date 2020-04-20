e3cc917454884b483bf548b1a1949bf34b743f04
Simon Maynard <snmaynard@gmail.com>
2020-04-20 15:49:55 -0700

    Add comment

--- features/fixtures/ios-swift-cocoapods/iOSTestApp/scenarios/SessionOOMScenario.m
+++ features/fixtures/ios-swift-cocoapods/iOSTestApp/scenarios/SessionOOMScenario.m
@@ -10,6 +10,7 @@
 }
 
 - (void)run {
+    // This test has determinism issues with ordering of payloads and batching of event payloads
     [Bugsnag startSession];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [Bugsnag notify:[NSException exceptionWithName:@"foo" reason:nil userInfo:nil]];
--- features/fixtures/ios-swift-cocoapods/iOSTestApp/scenarios/StopSessionOOMScenario.m
+++ features/fixtures/ios-swift-cocoapods/iOSTestApp/scenarios/StopSessionOOMScenario.m
@@ -10,6 +10,7 @@
 
 - (void)run {
     [Bugsnag startSession];
+    // This test has determinism issues with ordering of payloads and batching of event payloads
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [Bugsnag notify:[NSException exceptionWithName:@"foo" reason:nil userInfo:nil]];
         [Bugsnag stopSession];
