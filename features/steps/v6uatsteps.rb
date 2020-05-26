edeccdfb880a68c322d48914eac634887c1cc4d5
Robin Macharg <robin.macharg+github@gmail.com>
2020-05-26 10:53:50 +0100

    UAT: mazerunner matchers.  Address Review feedback

--- features/steps/v6uatsteps.rb
+++ features/steps/v6uatsteps.rb
@@ -7,21 +7,11 @@ Then(/^the payload field "(.+)" is a number$/) do |field_path|
     assert_kind_of Numeric, value
 end
 
-Then(/^the payload field "(.+)" is not a number$/) do |field_path|
-    value = read_key_path(Server.current_request[:body], field_path)
-    assert_false(value.is_a?(Integer) || value.is_a?(Float))
-end
-
 # Float
 
 Then(/^the payload field "(.+)" is a float$/) do |field_path|
     value = read_key_path(find_request(request_index)[:body], field_path)
     assert_true(value.is_a?(Float))
-end
-
-Then(/^the payload field "(.+)" is not a float$/) do |field_path|
-    value = read_key_path(find_request(request_index)[:body], field_path)
-    assert_false(value.is_a?(Float))
 end
 
 # Integer
@@ -31,22 +21,10 @@ Then(/^the payload field "(.+)" is an integer$/) do |field_path|
     assert_true(value.is_a?(Integer))
 end
 
-Then(/^the payload field "(.+)" is not an integer$/) do |field_path|
-    value = read_key_path(Server.current_request[:body], field_path)
-    assert_false(value.is_a?(Integer))
-end
-
 # Date
 
 Then(/^the payload field "(.+)" is a date$/) do |field_path|
     value = read_key_path(Server.current_request[:body], field_path)
     date = Date.parse(value) rescue nil
     assert_true(date.is_a?(Date))
-end
-
-Then(/^the payload field "(.+)" is not a date$/) do |field_path|
-    value = read_key_path(Server.current_request[:body], field_path)
-    date = nil
-    date = Date.parse(value)
-    assert_false(date.is_a?(Date))
 end
