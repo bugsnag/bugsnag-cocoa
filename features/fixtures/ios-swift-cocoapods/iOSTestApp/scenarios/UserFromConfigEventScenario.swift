bb3e22ab1af7776233e653fd0c78a06e81ca087e
fractalwrench <fractalwrench@gmail.com>
2020-05-27 09:45:29 +0100

    test: add E2E tests for verifying user info

---
+++ features/fixtures/ios-swift-cocoapods/iOSTestApp/scenarios/UserFromConfigEventScenario.swift
@@ -0,0 +1,32 @@
+//
+//  UserFromConfigEventScenario.swift
+//  iOSTestApp
+//
+//  Created by Jamie Lynch on 26/05/2020.
+//  Copyright © 2020 Bugsnag. All rights reserved.
+//
+
+import Foundation
+import Bugsnag
+
+/**
+ * Sends an event  to Bugsnag which contains a user set from Configuration
+ */
+internal class UserFromConfigEventScenario: Scenario {
+
+    override func startBugsnag() {
+        self.config.autoTrackSessions = false;
+        self.config.setUser("abc", withEmail: "fake@gmail.com", andName: "Fay K")
+        super.startBugsnag()
+    }
+
+    override func run() {
+        let user = Bugsnag.user()
+        Bugsnag.addMetadata(user.id, key: "id", section: "clientUserValue")
+        Bugsnag.addMetadata(user.email, key: "email", section: "clientUserValue")
+        Bugsnag.addMetadata(user.name, key: "name", section: "clientUserValue")
+
+        let error = NSError(domain: "UserFromConfigScenario", code: 100, userInfo: nil)
+        Bugsnag.notifyError(error)
+    }
+}
