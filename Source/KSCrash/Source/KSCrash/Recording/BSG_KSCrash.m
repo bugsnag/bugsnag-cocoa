d5b28d107bc63580c28bb63726e19a6a549631d6
Robin Macharg <robin.macharg+github@gmail.com>
2020-06-03 22:13:36 +0100

    fix: the DYLD cache locking mechanism now determines the lock mechanism at runtime, not compile-time

--- Source/KSCrash/Source/KSCrash/Recording/BSG_KSCrash.m
+++ Source/KSCrash/Source/KSCrash/Recording/BSG_KSCrash.m
@@ -280,10 +280,11 @@ IMPLEMENT_EXCLUSIVE_SHARED_INSTANCE(BSG_KSCrash)
  * behaviour.
  */
 - (void)listenForLoadedBinaries {
+    // Store a static version of the OS for comparison efficiency
+    bsg_initialise_static_os();
     bsg_initialise_mach_binary_headers(BSG_INITIAL_MACH_BINARY_IMAGE_ARRAY_SIZE);
 
-    // Note: Internally, access to DYLD's binary image store is guarded by an OSSpinLock.  We therefore don't need to
-    // add additional guards around our access.
+    // Note: Access to DYLD's binary image store is guarded by locks.
     _dyld_register_func_for_remove_image(&bsg_mach_binary_image_removed);
     _dyld_register_func_for_add_image(&bsg_mach_binary_image_added);
 }
