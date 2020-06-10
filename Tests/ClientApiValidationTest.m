0c572095ac96957f1d6e08d840fdb02df19270c1
fractalwrench <fractalwrench@gmail.com>
2020-06-10 11:45:55 +0100

    test: add public API validation

---
+++ Tests/ClientApiValidationTest.m
@@ -0,0 +1,29 @@
+//
+//  ClientApiValidationTest.m
+//  Bugsnag
+//
+//  Created by Jamie Lynch on 10/06/2020.
+//  Copyright © 2020 Bugsnag Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+/**
+* Validates that the Client API interface handles any invalid input gracefully.
+*/
+@interface ClientApiValidationTest : XCTestCase
+
+@end
+
+@implementation ClientApiValidationTest
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
