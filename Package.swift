f2ffbe6e35815d1d7a1541e0ec40627f498d7c36
Joe Haines <hello@joehaines.co.uk>
2020-06-26 15:43:55 +0100

    Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,57 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "Bugsnag",
+    platforms: [
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .iOS(.v9),
+    ],
+    products: [
+        .library(name: "Bugsnag", targets: ["Bugsnag"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Bugsnag",
+            dependencies: [],
+            path: "Bugsnag",
+            sources: [
+                ".",
+                "Breadcrumbs",
+                "Client",
+                "Configuration",
+                "Delivery",
+                "Helpers",
+                "KSCrash",
+                "Metadata",
+                "Payload",
+                "Plugins",
+                "KSCrash/Source/KSCrash/Reporting/Filters",
+                "KSCrash/Source/KSCrash/Recording/Tools",
+                "KSCrash/Source/KSCrash/Recording",
+                "KSCrash/Source/KSCrash/Recording/Sentry",
+            ],
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("."),
+                .headerSearchPath("include/Bugsnag"),
+                .headerSearchPath("Breadcrumbs"),
+                .headerSearchPath("Client"),
+                .headerSearchPath("Configuration"),
+                .headerSearchPath("Delivery"),
+                .headerSearchPath("Helpers"),
+                .headerSearchPath("KSCrash"),
+                .headerSearchPath("Metadata"),
+                .headerSearchPath("Payload"),
+                .headerSearchPath("Plugins"),
+                .headerSearchPath("Storage"),
+                .headerSearchPath("KSCrash/Source/KSCrash/Reporting/Filters"),
+                .headerSearchPath("KSCrash/Source/KSCrash/Recording/Tools"),
+                .headerSearchPath("KSCrash/Source/KSCrash/Recording"),
+                .headerSearchPath("KSCrash/Source/KSCrash/Recording/Sentry"),
+            ]
+        ),
+    ]
+)
