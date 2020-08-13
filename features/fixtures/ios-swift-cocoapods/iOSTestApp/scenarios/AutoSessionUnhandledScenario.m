7e62c8d8d04fb92d367521867c0132bca07c0f5f
Alex Moinet <alex@bugsnag.com>
2020-08-13 14:50:54 +0100

    Tests: Decrease delay between session and error to fix currently flaky test

--- features/fixtures/ios-swift-cocoapods/iOSTestApp/scenarios/AutoSessionUnhandledScenario.m
+++ features/fixtures/ios-swift-cocoapods/iOSTestApp/scenarios/AutoSessionUnhandledScenario.m
@@ -21,7 +21,7 @@
 
 - (void)run {
     if (![self.eventMode isEqualToString:@"noevent"]) {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 4 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
             NSException *ex = [NSException exceptionWithName:@"Kaboom" reason:@"The connection exploded" userInfo:nil];
 
             @throw ex;
