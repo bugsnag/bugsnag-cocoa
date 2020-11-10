1231454ed641bae1590dbc1ea526ffe52712f65b
Nick Dowell <nick@bugsnag.com>
2020-11-10 08:46:17 +0000

    Prepare fixtures for running Mac E2E tests

--- features/fixtures/macos/macOSTestApp/main.m
+++ features/fixtures/macos/macOSTestApp/main.m
@@ -9,5 +9,15 @@
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
+    [[NSUserDefaults standardUserDefaults] registerDefaults:@{
+        // Disable state restoration to prevent the following dialog being shown after crashes
+        // "The last time you opened macOSTestApp, it unexpectedly quit while reopening windows.
+        //  Do you want to try to reopen its windows again?"
+        // https://developer.apple.com/library/archive/releasenotes/AppKit/RN-AppKitOlderNotes/index.html#10_7StateRestoration
+        @"ApplePersistenceIgnoreState": @YES,
+        // Stop NSApplication swallowing NSExceptions thrown on the main thread.
+        @"NSApplicationCrashOnExceptions": @YES,
+    }];
+    
     return NSApplicationMain(argc, argv);
 }
