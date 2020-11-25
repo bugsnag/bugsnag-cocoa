11c2c539be895c6abced92a0a9fe4aeb218a3ce1
Nick Dowell <nick@bugsnag.com>
2020-11-25 14:25:18 +0000

    Release 6.2.6

--- Bugsnag/Payload/BugsnagNotifier.m
+++ Bugsnag/Payload/BugsnagNotifier.m
@@ -23,7 +23,7 @@
 #else
         self.name = @"Bugsnag Objective-C";
 #endif
-        self.version = @"6.2.5";
+        self.version = @"6.2.6";
         self.url = @"https://github.com/bugsnag/bugsnag-cocoa";
         self.dependencies = [NSMutableArray new];
     }
