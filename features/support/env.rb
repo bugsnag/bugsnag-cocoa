905d1224d83dcfa41c986b9ebecf6ce08b8d03d9
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-11-25 13:34:06 +0000

    Configure Maze to suppress integrity header check

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,8 @@
 # Set this explicitly
 $api_key = "12312312312312312312312312312312"
+
+AfterConfiguration do |_config|
+  MazeRunner.config.receive_no_requests_wait = 15
+  # TODO: Remove once the Bugsnag-Integrity header has been implemented
+  MazeRunner.config.enforce_bugsnag_integrity = false
+end
