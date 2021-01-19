892221a14bdb2370cbf02d288e712d7288bf9cfd
Alex Moinet <alex@bugsnag.com>
2021-01-19 15:10:58 +0000

    Tests/MacOS pipeline: Add zip/app file processing to env file

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,28 @@
+require 'fileutils'
+
 # Set this explicitly
 $api_key = "12312312312312312312312312312312"
 
 AfterConfiguration do |_config|
   Maze.config.receive_no_requests_wait = 15
+end
+
+# Additional require MacOS configuration
+if MazeRunner.config.os == 'macos'
+  fixture_dir = 'features/fixtures/macos/output'
+  app_dir = '/Applications'
+  zip_name = "#{MazeRunner.config.app}.zip"
+  app_name = "#{MazeRunner.config.app}.app"
+
+  # If built app file already exists, skip unzip
+  unless File.exists?("#{fixture_dir}/#{app_name}")
+    raise Exception, 'Test fixture build archive not found' unless File.file?("#{fixture_dir}/#{zip_name}")
+    `unzip #{fixture_dir}/#{zip_name}`
+  end
+
+  FileUtils.mv("#{fixture_dir}/#{app_name}", "#{app_dir}/#{app_name}")
+
+  at_exit do
+    FileUtils.rm_rf("#{app_dir}/#{app_name}")
+  end
 end
