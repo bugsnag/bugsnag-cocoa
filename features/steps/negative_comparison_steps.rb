ff4e52dcc7b6407c176241b0fbb39c1b84f5b705
Steve Kirkland <twometresteve@users.noreply.github.com>
2021-01-19 23:14:54 +0000

    Update compare routine name [full ci]

--- features/steps/negative_comparison_steps.rb
+++ features/steps/negative_comparison_steps.rb
@@ -1,6 +1,6 @@
 Then("the error payload field {string} does not equal {string}") do |field_path, string_value|
   payload_value = Maze::Helper.read_key_path(Maze::Server.errors.current[:body], field_path)
-  result = value_compare(payload_value, string_value)
+  result = Maze::Compare.value(payload_value, string_value)
   assert_false(result.equal?, "Value: #{string_value} equals payload element at: #{field_path}")
 end
 
