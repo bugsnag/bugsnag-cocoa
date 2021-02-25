6c950db587f3007bba5295b1e60daa297b5f5cfb
Nick Dowell <nick@bugsnag.com>
2021-02-25 10:50:50 +0000

    Stop using KVO to determine current uploads

--- Bugsnag/Delivery/BSGEventUploader.m
+++ Bugsnag/Delivery/BSGEventUploader.m
@@ -17,9 +17,6 @@
 
 @interface BSGEventUploader () <BSGEventUploadOperationDelegate>
 
-/// The files currently being uploaded, used to prevent duplicate uploads.
-@property (readonly, nonatomic) NSMutableSet<NSString *> *currentFiles;
-
 @property (readonly, nonatomic) NSString *eventsDirectory;
 
 @property (readonly, nonatomic) NSString *kscrashReportsDirectory;
@@ -37,64 +34,22 @@
 @synthesize configuration = _configuration;
 @synthesize notifier = _notifier;
 
-static void *OperationsContext = &OperationsContext;
-
 - (instancetype)initWithConfiguration:(BugsnagConfiguration *)configuration notifier:(BugsnagNotifier *)notifier {
     if (self = [super init]) {
         _apiClient = [[BugsnagApiClient alloc] initWithSession:configuration.session queueName:@""];
         _configuration = configuration;
-        _currentFiles = [[NSMutableSet alloc] init];
         _eventsDirectory = [BSGFileLocations current].events;
         _kscrashReportsDirectory = [BSGFileLocations current].kscrashReports;
         _notifier = notifier;
         _operationQueue = [[NSOperationQueue alloc] init];
         _operationQueue.maxConcurrentOperationCount = 1;
         _operationQueue.name = @"com.bugsnag.event-uploader";
-        [_operationQueue addObserver:self forKeyPath:NSStringFromSelector(@selector(operations))
-                             options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld
-                             context:OperationsContext];
     }
     return self;
 }
 
 - (void)dealloc {
-    [_operationQueue removeObserver:self forKeyPath:NSStringFromSelector(@selector(operations)) context:OperationsContext];
     [_operationQueue cancelAllOperations];
-}
-
-// KVO is used to ensure `currentFiles` always reflects the state of the operation queue.
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
-    if (context == OperationsContext) {
-        NSMutableArray *addedOperations = [change[NSKeyValueChangeNewKey] mutableCopy];
-        [addedOperations removeObjectsInArray:change[NSKeyValueChangeOldKey]];
-        
-        NSMutableArray *finishedOperations = [change[NSKeyValueChangeOldKey] mutableCopy];
-        [finishedOperations removeObjectsInArray:change[NSKeyValueChangeNewKey]];
-        
-        NSMutableSet<NSString *> *addedFiles = [NSMutableSet set];
-        NSMutableSet<NSString *> *finishedFiles = [NSMutableSet set];
-        
-        for (id operation in addedOperations) {
-            if ([operation isKindOfClass:[BSGEventUploadOperation class]]) {
-                if ([operation isKindOfClass:[BSGEventUploadFileOperation class]]) {
-                    [addedFiles addObject:((BSGEventUploadFileOperation *)operation).file];
-                }
-            }
-        }
-        
-        for (id operation in finishedOperations) {
-            if ([operation isKindOfClass:[BSGEventUploadFileOperation class]]) {
-                [finishedFiles addObject:((BSGEventUploadFileOperation *)operation).file];
-            }
-        }
-        
-        if (addedFiles.count || finishedFiles.count) {
-            @synchronized (self.currentFiles) {
-                [self.currentFiles unionSet:addedFiles];
-                [self.currentFiles minusSet:finishedFiles];
-            }
-        }
-    }
 }
 
 - (void)uploadEvent:(BugsnagEvent *)event {
@@ -128,9 +83,11 @@ static void *OperationsContext = &OperationsContext;
     
     NSMutableDictionary<NSString *, NSDate *> *creationDates = [NSMutableDictionary dictionary];
     
-    NSArray *currentFiles;
-    @synchronized (self.currentFiles) {
-        currentFiles = self.currentFiles.allObjects;
+    NSMutableArray *currentFiles = [NSMutableArray array];
+    for (id operation in self.operationQueue.operations) {
+        if ([operation isKindOfClass:[BSGEventUploadFileOperation class]]) {
+            [currentFiles addObject:((BSGEventUploadFileOperation *)operation).file];
+        }
     }
     
     for (NSString *directory in @[self.eventsDirectory, self.kscrashReportsDirectory]) {
