fdfbfe177abf3a51d06f0ceb074edd006a6ed922
Nick Dowell <nick@bugsnag.com>
2021-08-09 10:38:09 +0100

    Add UIScene life-cycle to examples/swift-ios

--- examples/swift-ios/swift-ios/AppDelegate.swift
+++ examples/swift-ios/swift-ios/AppDelegate.swift
@@ -87,5 +87,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         return true
     }
+    
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
 }
 
