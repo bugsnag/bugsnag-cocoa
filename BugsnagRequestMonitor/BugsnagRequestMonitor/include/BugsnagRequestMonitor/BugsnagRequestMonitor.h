8301de2c561019df449bce45d1454397623d6dee
Karl Stenerud <kstenerud@gmail.com>
2021-08-26 13:45:07 +0200

    Add base network breadcrumbs plugin scaffolding

---
+++ BugsnagRequestMonitor/BugsnagRequestMonitor/include/BugsnagRequestMonitor/BugsnagRequestMonitor.h
@@ -0,0 +1,9 @@
+//
+//  BugsnagRequestMonitor.h
+//  BugsnagRequestMonitor
+//
+//  Created by Karl Stenerud on 26.08.21.
+//
+
+#import <Foundation/Foundation.h>
+#import <Bugsnag/Bugsnag.h>
--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
     ],
     products: [
         .library(name: "Bugsnag", targets: ["Bugsnag"]),
+        .library(name: "BugsnagRequestMonitor", targets: ["BugsnagRequestMonitor"]),
     ],
     dependencies: [],
     targets: [
@@ -39,6 +40,16 @@ let package = Package(
             linkerSettings: [
                 .linkedLibrary("z"),
                 .linkedLibrary("c++"),
+            ]
+        ),
+        .target(
+            name: "BugsnagRequestMonitor",
+            dependencies: ["Bugsnag"],
+            path: "BugsnagRequestMonitor/BugsnagRequestMonitor",
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("."),
+                .headerSearchPath("include/BugsnagRequestMonitor"),
             ]
         ),
     ],
