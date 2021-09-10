b9f1794dc304c1d78336cb611a1c6687271ed726
Steve Kirkland-Walton <steve@bugsnag.com>
2021-09-10 11:39:33 +0100

    Add breadcrumbs module to iOS test fixture

--- features/fixtures/shared/scenarios/NetworkBreadcrumbsScenario.swift
+++ features/fixtures/shared/scenarios/NetworkBreadcrumbsScenario.swift
@@ -6,23 +6,37 @@
 //  Copyright © 2021 Bugsnag. All rights reserved.
 //
 
+import BugsnagRequestMonitor
 import Foundation
 
 class NetworkBreadcrumbsScenario : Scenario {
 
     override func startBugsnag() {
         self.config.autoTrackSessions = false;
-
+        self.config.add(BugsnagRequestMonitor())
+        
         super.startBugsnag()
     }
 
     override func run() {
 
         // Make some network requests so that automatic network breadcrumbs are left
-        
-        
+        query(address: "http://bs-local.com:9340/?status=444")
+        query(address: "http://bs-local.com:9340/?delay_ms=3000")
+
         // Send a handled error
         let error = NSError(domain: "NetworkBreadcrumbsScenario", code: 100, userInfo: nil)
         Bugsnag.notifyError(error)
+    }
+    
+    func query(address: String) {
+        let url = URL(string: address)!
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            semaphore.signal()
+        }
+        task.resume()
+        semaphore.wait()
     }
 }
