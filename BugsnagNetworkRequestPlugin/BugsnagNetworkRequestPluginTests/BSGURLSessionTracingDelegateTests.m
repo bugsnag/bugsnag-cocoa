5883a9e1d975430eaa90e7e6ec6ea2fb2608a0fc
Nick Dowell <nick@bugsnag.com>
2021-09-22 15:44:27 +0100

    Add more query item test samples
    https://github.com/bugsnag/bugsnag-cocoa/pull/1193#discussion_r713945673

--- BugsnagNetworkRequestPlugin/BugsnagNetworkRequestPluginTests/BSGURLSessionTracingDelegateTests.m
+++ BugsnagNetworkRequestPlugin/BugsnagNetworkRequestPluginTests/BSGURLSessionTracingDelegateTests.m
@@ -32,6 +32,14 @@ XCTAssertEqualObjects([BSGURLSessionTracingDelegate urlParamsForQueryItems:[NSUR
     
     // Query items with no value should be represented as empty string.
     TEST(@"http://example.com?foo=bar&foo=baz&foo=&sort=name", (@{@"foo": @[@"bar", @"baz", @""], @"sort": @"name"}));
+    
+    TEST(@"http://example.com?foo", @{});
+    
+    TEST(@"http://example.com?=bar", @{@"": @"bar"});
+    
+    TEST(@"http://example.com?foo=bar&", @{@"foo": @"bar"});
+    
+    TEST(@"http://example.com?foo=bar&baz", @{@"foo": @"bar"});
 
 #undef TEST
 }
