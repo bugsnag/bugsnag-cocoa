c21b1a08480da3c3d7654f0f2c63ab456f92949c
Nick Dowell <nick@bugsnag.com>
2021-11-24 13:23:14 +0000

    Don't delete kscrash.log upon relaunch

--- features/fixtures/ios/iOSTestApp/iOSTestApp-Bridging-Header.h
+++ features/fixtures/ios/iOSTestApp/iOSTestApp-Bridging-Header.h
@@ -7,3 +7,9 @@
 #import <BugsnagNetworkRequestPlugin/BugsnagNetworkRequestPlugin.h>
 
 extern bool bsg_kslog_setLogFilename(const char *filename, bool overwrite);
+
+extern void bsg_i_kslog_logCBasic(const char *fmt, ...) __printflike(1, 2);
+
+static inline void kslog(const char *message) {
+    bsg_i_kslog_logCBasic("%s", message);
+}
