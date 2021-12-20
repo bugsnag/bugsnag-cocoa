b9fea65348c0c8468c5422bf9fed957baf9420d6
Nick Dowell <nick@bugsnag.com>
2021-12-20 10:35:06 +0000

    Work around Appium errors in app hang tests

--- features/fixtures/shared/scenarios/AppHangScenarios.swift
+++ features/fixtures/shared/scenarios/AppHangScenarios.swift
@@ -70,7 +70,10 @@ class AppHangFatalOnlyScenario: Scenario {
     
     override func run() {
         NSLog("Hanging indefinitely...")
-        while true {}
+        // Use asyncAfter to allow the Appium click event to be handled
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            while true {}
+        }
     }
 }
 
@@ -84,7 +87,10 @@ class AppHangFatalDisabledScenario: Scenario {
     
     override func run() {
         NSLog("Hanging indefinitely...")
-        while true {}
+        // Use asyncAfter to allow the Appium click event to be handled
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            while true {}
+        }
     }
 }
 
