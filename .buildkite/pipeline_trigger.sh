5682d4e19c44ddef7806e90961a1fdf745c852f1
Steve Kirkland-Walton <steve@bugsnag.com>
2022-05-19 17:51:21 +0100

    Consolidate “barebones” and “quick” builds into “basic”

--- .buildkite/pipeline_trigger.sh
+++ .buildkite/pipeline_trigger.sh
@@ -5,10 +5,8 @@ if [[ "$BUILDKITE_MESSAGE" == *"[full ci]"* ||
   ! -z "$FULL_SCHEDULED_BUILD" ]]; then
   echo "Running full build"
   buildkite-agent pipeline upload .buildkite/pipeline.full.yml
-  buildkite-agent pipeline upload .buildkite/pipeline.quick.yml
 else
   # Basic build, but allow a full build to be triggered
   echo "Running basic build"
   buildkite-agent pipeline upload .buildkite/block.full.yml
-  buildkite-agent pipeline upload .buildkite/pipeline.quick.yml
 fi
