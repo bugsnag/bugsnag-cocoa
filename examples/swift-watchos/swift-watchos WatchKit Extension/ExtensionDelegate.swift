209a84cf61d18cf35e3e5df7cc552d2145f6ae7a
Karl Stenerud <kstenerud@gmail.com>
2022-05-20 15:32:46 +0200

    Allow session tracking on watchos even though it's technically an extension

--- examples/swift-watchos/swift-watchos WatchKit Extension/ExtensionDelegate.swift
+++ examples/swift-watchos/swift-watchos WatchKit Extension/ExtensionDelegate.swift
@@ -7,6 +7,7 @@
 
 import WatchKit
 import Bugsnag
+import BugsnagNetworkRequestPlugin
 
 class ExtensionDelegate: NSObject, WKExtensionDelegate {
 
@@ -26,7 +27,6 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
         
         // Create config object from the application plist
 //      let config = BugsnagConfiguration.loadConfig()
-        
         // ... or construct an empty object
 //      let config = BugsnagConfiguration("YOUR-API-KEY")
 
