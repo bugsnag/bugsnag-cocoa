52b0b6f3033ed88e4fa748cb0c11f551095d0a52
Nick Dowell <nick@bugsnag.com>
2022-07-06 16:05:09 +0100

    Support E2E code coverage on macOS (#1426)

--- features/scripts/export_mac_app.sh
+++ features/scripts/export_mac_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -o errexit
+set -euo pipefail
 
 cd features/fixtures/macos
 
@@ -10,15 +10,26 @@ pod install
 
 echo "--- macOSTestApp: xcodebuild archive"
 
-xcrun xcodebuild \
-  -workspace macOSTestApp.xcworkspace \
-  -scheme macOSTestApp \
-  -destination generic/platform=macOS \
-  -configuration Debug \
-  -archivePath archive/macOSTestApp.xcarchive \
-  -quiet \
-  archive \
+BUILD_ARGS=(
+  -workspace macOSTestApp.xcworkspace
+  -scheme macOSTestApp
+  -destination generic/platform=macOS
+  -configuration Debug
+  -archivePath archive/macOSTestApp.xcarchive
+  -quiet
+  archive
   ONLY_ACTIVE_ARCH=NO
+)
+
+if [ "${ENABLE_CODE_COVERAGE:-}" = YES ]; then
+  BUILD_ARGS+=(
+    OTHER_CFLAGS='$(inherited) -fprofile-instr-generate -fcoverage-mapping'
+    OTHER_LDFLAGS='$(inherited) -fprofile-instr-generate'
+    OTHER_SWIFT_FLAGS='$(inherited) -profile-generate -profile-coverage-mapping'
+  )
+fi
+
+xcodebuild "${BUILD_ARGS[@]}"
 
 echo "--- macOSTestApp: xcodebuild -exportArchive"
 
@@ -34,4 +45,4 @@ cd output
 
 echo "--- macOSTestApp: zip"
 
-zip -r macOSTestApp.zip macOSTestApp.app
+zip -qr macOSTestApp.zip macOSTestApp.app
