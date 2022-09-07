85b0e09b7ca1e2f72610a3ef77dc6e082f1838c8
Nick Dowell <nick@bugsnag.com>
2022-09-07 08:45:09 +0100

    [PLAT-8866] Add E2E test for Internal Error at startup (#1477)

---
+++ features/fixtures/shared/scenarios/CouldNotCreateDirectoryScenario.swift
@@ -0,0 +1,35 @@
+//
+//  CouldNotCreateDirectoryScenario.swift
+//  macOSTestApp
+//
+//  Created by Nick Dowell on 05/09/2022.
+//  Copyright © 2022 Bugsnag Inc. All rights reserved.
+//
+
+class CouldNotCreateDirectoryScenario: Scenario {
+    
+    override func startBugsnag() {
+        // Prevent Bugsnag from creating its subdirectories
+        
+        Scenario.clearPersistentData()
+        
+        let fileManager = FileManager()
+        
+        let dir = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+            .appendingPathComponent("com.bugsnag.Bugsnag")
+            .appendingPathComponent(Bundle.main.bundleIdentifier!)
+        
+        try? fileManager.removeItem(at: dir)
+        
+        do {
+            try fileManager.createDirectory(at: dir, withIntermediateDirectories: true)
+            try fileManager.setAttributes([.posixPermissions: 0o600], ofItemAtPath: dir.path)
+            super.startBugsnag()
+        } catch {
+            NSLog("\(error)")
+        }
+    }
+    
+    override func run() {
+    }
+}
