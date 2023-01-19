4c356ab5a9ba5a7146d514901daca147ad37dfb8
matth-bugsnag <113024072+matth-bugsnag@users.noreply.github.com>
2023-01-19 11:28:56 +0000

    onError blocked from running if releaseStage not in enabledReleaseStages
    fixing build error
    
    onError will run when NO enabledReleaseStage set
    
    Refactoring releaseStage check
    
    Updated changelog

--- Bugsnag/Client/BugsnagClient.m
+++ Bugsnag/Client/BugsnagClient.m
@@ -747,6 +747,12 @@ BSG_OBJC_DIRECT_MEMBERS
 - (void)notifyInternal:(BugsnagEvent *_Nonnull)event
                  block:(BugsnagOnErrorBlock)block
 {
+    // Checks whether releaseStage is in enabledReleaseStages, blocking onError callback from running if it is not.
+    if (!self.configuration.shouldSendReports || ![event shouldBeSent]) {
+        bsg_log_info("Discarding error because releaseStage '%@' not in enabledReleaseStages", self.configuration.releaseStage);
+        return;
+    }
+    
     NSString *errorClass = event.errors.firstObject.errorClass;
     if ([self.configuration shouldDiscardErrorClass:errorClass]) {
         bsg_log_info(@"Discarding event because errorClass \"%@\" matched configuration.discardClasses", errorClass);
