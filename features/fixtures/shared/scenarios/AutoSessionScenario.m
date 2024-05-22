1f1dee0e3bad1eed1f1447fd0d4c2f374d20ab94
Karl Stenerud <kstenerud@gmail.com>
2024-05-22 09:57:24 +0200

    Use app deep links

--- features/fixtures/shared/scenarios/AutoSessionScenario.m
+++ features/fixtures/shared/scenarios/AutoSessionScenario.m
@@ -5,8 +5,9 @@
 
 #import "Scenario.h"
 #import "Logging.h"
+#if __has_include(<UIKit/UIKit.h>)
 #import <UIKit/UIKit.h>
-
+#endif
 /**
  * Sends an automatic session payload to Bugsnag.
  */
@@ -16,13 +17,15 @@
 @implementation AutoSessionScenario
 
 - (void)run {
+#if __has_include(<UIKit/UIKit.h>)
     if (@available(iOS 10.0, *)) {
-        NSURL *url = [NSURL URLWithString: @"http://bs-local.com:9339/docs/test.html"];
+        NSURL *url = [NSURL URLWithString: @"http://bs-local.com:9339/docs/test.html?next=https://google.com"];
         
         [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:^(BOOL success) {
             NSLog(@"Opened %@ with success %d", url, success);
         }];
     }
+#endif
 }
 
 @end
