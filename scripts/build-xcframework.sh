695e61640103deda58ecc59e13b0e450e9c2079a
Karl Stenerud <kstenerud@gmail.com>
2024-07-09 10:11:14 +0200

    New universal xcframework builder script (harmonized between bugsnag-cocoa and bugsnag-cocoa-performance)

--- scripts/build-xcframework.sh
+++ scripts/build-xcframework.sh
@@ -15,16 +15,17 @@ set -e -u -o pipefail
 # Configuration
 # -------------------------------------------------------------------
 
-# Base name of the .xcodeproj directory
-PROJECT_NAME=Bugsnag
+# Framework:                The base name we want to give our xcframework.
+# Project / Workspace Path: Local path to the .xcodeproj or .xcworkspace we want to load from
+# Scheme:                   The base name of the scheme to build with (minus any "-iOS" type suffix)
+# Namespaced:               If TRUE, the schemes to load all have an ("-iOS", "-macOS") style namespaced suffix.
+TARGETS=(
+#   [Framework]                     [Project/WS Path]      [Scheme]                       [NAMESPACED]
+    "Bugsnag                        Bugsnag.xcworkspace    Bugsnag                        TRUE"
+    "BugsnagNetworkRequestPlugin    Bugsnag.xcworkspace    BugsnagNetworkRequestPlugin    TRUE"
+)
 
-# Targets that are suffixed with the platform (e.g. XYZ-iOS, XYZ-macOS etc)
-NAMESPACED_TARGETS=( "Bugsnag" )
-
-# Targets that are not namespaced
-GENERIC_TARGETS=(  )
-
-# Platforms we are building for: iOS, macOS, tvOS, watchOS (in future: visionOS)
+# Platforms we are building for
 PLATFORMS=( "iOS" "macOS" "tvOS" "watchOS" )
 
 
@@ -39,83 +40,109 @@ XCFW_DIR="${PROJECT_DIR}/build/xcframeworks"
 INTERMEDIATES_DIR="${XCFW_DIR}/intermediates"
 PRODUCTS_DIR="${XCFW_DIR}/products"
 
-rm -rf "${XCFW_DIR}"
-mkdir -p "${INTERMEDIATES_DIR}"
-mkdir -p "${PRODUCTS_DIR}"
-
 ##
-# Build a target. This function tries to sort out whether the target is namespaced or not
-# (i.e. if it's named XYZ or XYZ-iOS) so that it produces a consistently named output file
-# (such as XYZ-iOS.framework, XYZ-iOS-simulator.framework).
+# Build a framework.
 #
 # Args:
-#   1: Name of compilation target
-#   2: Name of platform to build for (iOS, macOS, etc)
+#   1: The base name of the framework to produce
+#   2: Path to the project or workspace file (relative to the project root)
+#   3: The scheme to build
+#   4: The platform to build for (iOS, macOS, etc)
 #
 # Return: A set of commandline argumets for the final framework build.
 ##
-build_target() {
-    target=$1
-    platform=$2
+build_framework() {
+    local framework_basename=$1
+    local proj_ws_path=$2
+    local scheme=$3
+    local platform=$4
 
-    framework_name=${target}
-    if [[ ${framework_name} == *${platform} ]]; then
-        # If our target is named "XYZ-iOS", remove the "-iOS" part from the framework name
-        framework_name="${framework_name%-${platform}}"
+    local destination="generic/platform=${platform}"
+    local archive_path="${INTERMEDIATES_DIR}/${framework_basename}-${platform}"
+    local proj_ws_arg="-project ${PROJECT_DIR}/${proj_ws_path}"
+    if [[ $proj_ws_path == *xcworkspace ]]; then
+        proj_ws_arg="-workspace ${PROJECT_DIR}/${proj_ws_path}"
     fi
 
-    destination="generic/platform=${platform}"
-    archive_path="${INTERMEDIATES_DIR}/${target}"
-    if [[ ${archive_path} != *${platform} ]]; then
-        # Make sure our archive path is namespaced with the platform
-        archive_path="${archive_path}-${platform}"
-    fi
     # Note: Redirecting xcodebuild's stdout to stderr because we need stdout for ourselves.
-    xcodebuild archive -project ${PROJECT_DIR}/${PROJECT_NAME}.xcodeproj \
-                       -scheme ${target} \
+    xcodebuild archive $proj_ws_arg \
+                       -scheme ${scheme} \
                        -configuration Release \
                        SKIP_INSTALL=NO \
                        BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
                        -destination "${destination}" \
                        -archivePath "${archive_path}" 1>&2
-    echo "-archive ${archive_path}.xcarchive -framework ${framework_name}.framework"
+    echo "-archive ${archive_path}.xcarchive -framework ${framework_basename}.framework"
 
     if [ "${platform}" != "macOS" ]; then
         # For non-macos targrets, build for the simulator as well.
         destination="${destination} Simulator"
         archive_path="${archive_path}-simulator"
-        xcodebuild archive -project ${PROJECT_DIR}/${PROJECT_NAME}.xcodeproj \
-                           -scheme ${target} \
+        xcodebuild archive $proj_ws_arg \
+                           -scheme ${scheme} \
                            -configuration Release \
                            SKIP_INSTALL=NO \
                            BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
                            -destination "${destination}" \
                            -archivePath "${archive_path}" 1>&2
-        echo "-archive ${archive_path}.xcarchive -framework ${framework_name}.framework"
+        echo "-archive ${archive_path}.xcarchive -framework ${framework_basename}.framework"
     fi
 }
 
-if [[ ${#NAMESPACED_TARGETS[@]} -gt 0 ]]; then
-    for target in "${NAMESPACED_TARGETS[@]}"; do
-        xcframework_args=""
-        for platform in "${PLATFORMS[@]}"; do
-            new_args=$( build_target ${target}-${platform} ${platform} )
-            xcframework_args="${xcframework_args} ${new_args}"
-        done
-        xcodebuild -create-xcframework ${xcframework_args} -output "${PRODUCTS_DIR}/${target}.xcframework"
-    done
-fi
+fixup_xcframework() {
+    # Swift compiler bug: Having a class with the same name as the module breaks things.
+    #
+    # The dependent module including such a module will generate class definitions like this:
+    #     "MyClass : MyModuleName.MyInterface {"
+    # When it should generate:
+    #     "MyClass : MyInterface {"
+    #
+    # So we search for this prefix and strip it out.
+    #
+    # https://developer.apple.com/forums/thread/123253
 
-if [[ ${#GENERIC_TARGETS[@]} -gt 0 ]]; then
-    for target in "${GENERIC_TARGETS[@]}"; do
-        xcframework_args=""
-        for platform in "${PLATFORMS[@]}"; do
-            new_args=$( build_target ${target} ${platform} )
-            xcframework_args="${xcframework_args} ${new_args}"
-        done
-        xcodebuild -create-xcframework ${xcframework_args} -output "${PRODUCTS_DIR}/${target}.xcframework"
+    local framework_basename="$1"
+    local framework_path="${PRODUCTS_DIR}/${framework_basename}.xcframework"
+
+    for target_record in "${TARGETS[@]}"; do
+        local target_args=($target_record)
+        local target_framework=${target_args[0]}
+
+        find "$framework_path" -name "*.swiftinterface" -exec sed -i -e "s/$target_framework\\.//g" {} \;
     done
-fi
+}
+
+build_frameworks() {
+    for target_record in "${TARGETS[@]}"; do
+        local target_args=($target_record)
+        local framework=${target_args[0]}
+        local project=${target_args[1]}
+        local scheme=${target_args[2]}
+        local namespaced=${target_args[3]}
+
+        echo "Building ${framework}.xcframework"
+        local xcframework_args=""
+        for platform in "${PLATFORMS[@]}"; do
+            local current_scheme="$scheme"
+            if [ "$namespaced" = "TRUE" ]; then
+                current_scheme="${current_scheme}-${platform}"
+            fi
+            local added_args=$( build_framework ${framework} ${project} ${current_scheme} ${platform} )
+            xcframework_args="${xcframework_args} ${added_args}"
+        done
+        xcodebuild -create-xcframework ${xcframework_args} -output "${PRODUCTS_DIR}/${framework}.xcframework"
+        fixup_xcframework $framework
+        pushd "${PRODUCTS_DIR}"
+        zip --symlinks -rq "${framework}.xcframework.zip" "${framework}.xcframework"
+        popd
+    done
+}
+
+rm -rf "${XCFW_DIR}"
+mkdir -p "${INTERMEDIATES_DIR}"
+mkdir -p "${PRODUCTS_DIR}"
+
+build_frameworks
 
 echo
 echo "** build-frameworks.sh: script completed successfully **"
